e10aeb0003fc663a180896a466e5af033caeacfd
김소은 <ildlisa@naver.com>
2025-10-29 17:16:48 +0900

    Title: 1부터의 합, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251029/1부터의 합/sum-from-the-one.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            sum += i;
+            if (sum >= N) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
