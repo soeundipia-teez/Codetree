75931347a3fac8332c92cefc0f887fc0570caf26
김소은 <ildlisa@naver.com>
2025-12-02 01:17:07 +0900

    Title: 특정 수 맞히기, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251202/특정 수 맞히기/catching-specific-number.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int num = sc.nextInt();
+            
+            if (num > 25) {
+                System.out.println("Lower");
+            } else if (num < 25) {
+                System.out.println("Higher");
+            } else {
+                System.out.println("Good");
+                break;
+            }
+        }
+    }
+}
